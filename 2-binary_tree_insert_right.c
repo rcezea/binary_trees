72649b7d0758f90d5176922addbc148321f3f41d
rcezea <rclancing@gmail.com>
2023-03-03 03:44:59 +0100

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,12 +9,16 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-if (parent == NULL || !new_node)
+if (!parent)
 {
 return (NULL);
 }
 new_node->parent = parent;
 new_node->n = value;
+if (!new_node)
+{
+return (NULL);
+}
 if (parent->right != NULL)
 {
 new_node->right = parent->right;
