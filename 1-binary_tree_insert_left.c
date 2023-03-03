3b5f46c6b88d149fbf9fcfcedb801495f8adf505
rcezea <rclancing@gmail.com>
2023-03-03 03:42:59 +0100

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-if (parent == NULL)
+if (!parent)
 {
 return (NULL);
 }
